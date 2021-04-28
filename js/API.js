6e1bc70c6369415447fbb180831063baa6a71758
Moon-Jang <roby8502@naver.com>
2021-04-28 16:17:05 +0900

    html 수정

--- js/API.js
+++ js/API.js
@@ -1,11 +1,26 @@
+const localStorage_KEY = "comments" 
 const fetchComments = () => {
     return fetch("../data/comments.json")
-    .then( response => response.json())
+    .then( response => {
+        const data = response.json()
+        localStorage.setItem(localStorage_KEY,data)
+        return data
+    });
 }
 
-const sendComments = () => {
-    
+const sendComments = ( name, comment) => {
+    const data = JSON.parse(localStorage.getItem(localStorage_KEY));
+    const newData = {
+        idx: data.length,
+        name,
+        comment
+    } 
+    data.push(newData)
+    localStorage.setItem(localStorage_KEY,data)
+    return data;
 }
+
 export default {
-    fetchComments
+    fetchComments,
+    sendComments
 }
